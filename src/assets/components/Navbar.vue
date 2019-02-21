48dbc5bbb20f63e672c264a34b24456a6c6aebcc
Panupong Ketrattanakul <paoyimpae@hotmail.com>
2019-02-21 21:53:40 +0700

    ☺ Add "SlideBar" in "NavBar" ♥

--- src/assets/components/Navbar.vue
+++ src/assets/components/Navbar.vue
@@ -1,9 +1,10 @@
 <template>
+  <div>
   <b-navbar toggleable="md" class="myNav">
     <!-- <b-navbar-toggle target="nav_collapse" /> -->
     <b-button
       variant="black"
-      @click.prevent=""
+      @click.prevent="isClick = !isClick"
       style="font-size:2rem;"
       class="text-warning mr-4"
       >&#9776;</b-button
@@ -36,6 +37,26 @@
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
+  <div v-show="isClick" id="menu">
+      <div class="inner">
+        <a class="close text-warning" @click="isClick = false" style="float: left; line-height: 2em;">X</a><br>
+        <ul class="links">
+          <li><b v-show="menu1" class="text-warning" style="margin-right: 5px;">></b>
+          <a @mouseover="menu1 = true" @mouseleave="menu1 = false" class="text-warning" href="#">Menu 1</a></li>
+          <li><b v-show="menu2" class="text-warning" style="margin-right: 5px;">></b>
+          <a @mouseover="menu2 = true" @mouseleave="menu2 = false" class="text-warning" href="#">Menu 2</a></li>
+          <li><b v-show="menu3" class="text-warning" style="margin-right: 5px;">></b>
+          <a @mouseover="menu3 = true" @mouseleave="menu3 = false" class="text-warning" href="#">Menu 3</a></li>
+          <li><b v-show="menu4" class="text-warning" style="margin-right: 5px;">></b>
+          <a @mouseover="menu4 = true" @mouseleave="menu4 = false" class="text-warning" href="#">Menu 4</a></li>
+          <li><b v-show="menu5" class="text-warning" style="margin-right: 5px;">></b>
+          <a @mouseover="menu5 = true" @mouseleave="menu5 = false" class="text-warning" href="#">Menu 5</a></li>
+          <li><b v-show="menu6" class="text-warning" style="margin-right: 5px;">></b>
+          <a @mouseover="menu6 = true" @mouseleave="menu6 = false" class="text-warning" href="#">Menu 6</a></li>
+        </ul>
+      </div>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -52,6 +73,8 @@ export default {
     return {
       isLoading: false,
       isFinish: true,
+      isClick: false,
+      menu1: false, menu2: false, menu3: false, menu4: false, menu5: false, menu6: false,
       date: moment()
         .tz('Asia/Bangkok')
         .format('YYYYMMDD') // return today as YEAR MONTH DAY format ex. 20191102
@@ -84,7 +107,7 @@ export default {
           alert('Signout Fail!')
           console.log(error)
         })
-    }
+    },
   },
   computed: {
     isAuth() {
@@ -107,5 +130,28 @@ export default {
 .myNav {
   background-color: black;
   margin: auto;
+}
+
+#menu {
+  background-color: black;
+  opacity: 0.8;
+  text-align: center;
+  position: fixed;
+  overflow: auto;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index:10;
+}
+
+.close {
+  margin-left: 10px;
+}
+
+.links {
+    list-style-type: none;
+    line-height: 6em;
+    font-size: 20px;
 }
 </style>
